ca4635b88ecd94e6629f34ac27a7f56b7d8b5c38
hutatBoosty <Parfenov.max24@yandex.ru>
2026-07-27 14:26:06 +0700

    delete comments

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import java.util.Comparator;
 import java.util.List;
 
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private static final String FILE_NAME = "students.txt";
 
